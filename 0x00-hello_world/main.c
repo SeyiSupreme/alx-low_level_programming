16dedb1ed0ca4e970fa216ba2be4d4921a5d4d1f
Seyi <adenijiseyi0@gmail.com>
2022-03-27 16:48:36 +0000

    jdjsakfad

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *main - Entry point
+ *
+ *Return: Always 0(success)
+ */
 int main(void)
 {
-	    return (0);
+	Return (0);
 }
